96bd7b80c703e9dedd7f14ee2dcc47a0c3aa9fe5
danielflorencio <danielflorencio20@gmail.com>
2023-01-26 09:13:26 -0300

    Updates

--- src/components/Footer/Footer.tsx
+++
@@ -1,8 +0,0 @@
-import './styles.css'
-export default function Footer(){
-    return(
-        <footer className='footer'>
-            <strong>Hey, nice to meet you, my name is Daniel 😃 and this is one of my portfolio projects. <a target="_blank" rel="noopener noreferrer" href="https://next-portfolio-pearl-five.vercel.app/">Click here</a> to know more about my work.</strong>
-        </footer>
-    )
-}
